5862a02a27ee7decbfc2e41bb2dce66061a4a2fb
Chris Aitchison <cmaitchison@gmail.com>
2013-02-26 14:30:28 +1100

    Add image fetching logic

--- lib/rails-file-icons.rb
+++ lib/rails-file-icons.rb
@@ -1,14 +1,27 @@
-require "rails_file_icons/version"
+require "rails-file-icons/engine"
 
-module RailsFileIcons
-  require "rails_file_icons/engine"
+module Icon
 
-  def self.icon_for_mime_type mime_type
-    'file_extension_doc.png'
+  KNOWN_EXTENSIONS = %w{
+    3gp 7z ace ai aif aiff amr asf asx bat bin bmp bup cab cbr cda cdl cdr chm
+    dat divx dll dmg doc dss dvf dwg eml eps exe fla flv gif gz hqx htm html
+    ifo indd iso jar jpeg jpg lnk log m4a m4b m4p m4v mcd mdb mid mov mp2 mp4
+    mpeg mpg msi mswmm ogg pdf png pps ps psd pst ptb pub qbb qbw qxd ram
+    rar rm rmvb rtf sea ses sit sitx ss swf tgz thm tif tmp torrent ttf txt
+    vcd vob wav wma wmv wps xls xpi zip
+    }.inject({}) do |known_extensions, ext|
+      known_extensions[ext] = "file_extension_#{ext}.png"
+      known_extensions
+    end
+  
+ 
+  
+  def self.for_filename filename
+    for_ext File.extname(filename)
   end
 
-  def self.icon_for_file_extension file_extension
-    'file_extension_ace.png'
+  def self.for_ext file_extension
+    ext = file_extension.start_with?('.') ? file_extension[1..-1] : file_extension
+    KNOWN_EXTENSIONS[ext.downcase] || 'file_extension_unknown.png'
   end
-
 end
--- spec/rails_file_icons_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Rails::File::Icons do
-  it 'should have a version number' do
-    Rails::File::Icons::VERSION.should_not be_nil
-  end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
-end
